4c60e3c154ed1226070891c59fa592e610c7028e
p2vnogud <22521147@gm.uit.edu.vn>
2025-10-20 02:01:06 +0700

    initial commit

---
+++ dllmain_attach/dll_thread_attach/dllmain.cpp
@@ -0,0 +1,33 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+__declspec(thread) int tls_var = 0; // ép hệ thống phải gọi DLL_THREAD_ATTACH
+
+void Log(const char* msg)
+{
+    char buf[256];
+    sprintf_s(buf, "[DLL] %s\n", msg);
+    OutputDebugStringA(buf); // an toàn trong DllMain
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        Log("PROCESS_ATTACH");
+        break;
+    case DLL_THREAD_ATTACH:
+        Log("THREAD_ATTACH");
+        break;
+    case DLL_THREAD_DETACH:
+        Log("THREAD_DETACH");
+        break;
+    case DLL_PROCESS_DETACH:
+        Log("PROCESS_DETACH");
+        break;
+    }
+    return TRUE;
+}
---
+++ dllmain_attach/thread_attach/thread_attach.cpp
@@ -0,0 +1,32 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+DWORD WINAPI ThreadProc(LPVOID lpParam)
+{
+    printf("[EXE] Thread started: %lu\n", GetCurrentThreadId());
+    Sleep(1000);
+    return 0;
+}
+
+int main()
+{
+    printf("[EXE] Main thread id: %lu\n", GetCurrentThreadId());
+
+    // Load DLL trước khi tạo thread
+    HMODULE h = LoadLibraryA("dll_thread_attach.dll");
+    if (!h) {
+        printf("LoadLibrary failed: %lu\n", GetLastError());
+        return 1;
+    }
+
+    // Tạo 3 thread mới
+    for (int i = 0; i < 3; i++) {
+        HANDLE th = CreateThread(NULL, 0, ThreadProc, 0, 0, 0);
+        CloseHandle(th);
+    }
+
+    Sleep(2000);
+    FreeLibrary(h);
+    printf("[EXE] Done.\n");
+    return 0;
+}
